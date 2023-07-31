ea17f5dee55cef29bd0172440b3073be215a5b2c
m-mattia-m <mattia@mattiamueggler.ch>
2023-07-31 22:17:12 +0200

    move plausible-initialization to main.ts

--- src/App.vue
+++ src/App.vue
@@ -7,21 +7,4 @@
 
 <script setup lang="ts">
 import Footer from "./components/Footer.vue";
-import Plausible from 'plausible-tracker'
-
-const plausibleDomain = import.meta.env.VITE_PLAUSIBLE_DOMAIN
-const plausibleHost = import.meta.env.VITE_PLAUSIBLE_HOST
-
-if (plausibleDomain == "" || plausibleHost == "") {
-  console.log(plausibleDomain)
-  console.log(plausibleHost)
-  throw new Error('Plausible could not be initialized!');
-}
-
-const { trackPageview } = Plausible({
-  apiHost: plausibleHost,
-  domain: plausibleDomain,
-  trackLocalhost: true,
-})
-trackPageview()
 </script>
