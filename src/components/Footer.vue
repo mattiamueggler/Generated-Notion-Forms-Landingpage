f3bdf8b02138604da6828bab8205df504e71d859
m-mattia-m <mattia@mattiamueggler.ch>
2023-08-01 21:54:10 +0200

    update router-link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-7xl overflow-hidden px-6 lg:px-8">
       <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
         <div v-for="item in navigation" :key="item.name" class="pb-6">
-          <a :href="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</a>
+          <router-link :to="item.href" class="text-sm leading-6 text-gray-600 hover:text-gray-900">{{ item.name }}</router-link>
         </div>
       </nav>
       <p class="mt-6 text-center text-xs leading-5 text-gray-500">&copy; {{ new Date().getFullYear() }} Generated Notion Forms from <a href="https://mattiamueggler.ch">mattiamueggler.ch.</a> All rights reserved.</p>
