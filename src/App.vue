dc9ac08626a573a8c358ad4cf352aee75c9ed493
m-mattia-m <mattia@mattiamueggler.ch>
2023-08-01 20:32:01 +0200

    add test env log

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,27 @@
 
 <script setup lang="ts">
 import Footer from "./components/Footer.vue";
+import Plausible from 'plausible-tracker'
+
+const mgApiUrl = import.meta.env.VITE_API_URL
+const plausibleDomain = import.meta.env.VITE_PLAUSIBLE_DOMAIN
+const plausibleHost = import.meta.env.VITE_PLAUSIBLE_HOST
+
+console.log("mgApiUrl: ")
+console.log(mgApiUrl)
+console.log(import.meta.env.VITE_PLAUSIBLE_HOST)
+console.log(import.meta.env.VITE_PLAUSIBLE_DOMAIN)
+
+if (plausibleDomain == "" || plausibleHost == "") {
+  throw new Error('Plausible could not be initialized!');
+}
+
+const { trackPageview } = Plausible({
+  apiHost: plausibleHost,
+  domain: plausibleDomain,
+  trackLocalhost: true,
+})
+trackPageview()
+
+
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,27 +2,7 @@ import { createApp } from 'vue'
 import './index.css'
 import { router } from './router';
 import App from './App.vue'
-import Plausible from 'plausible-tracker'
 
-const mgApiUrl = import.meta.env.VITE_API_URL
-const plausibleDomain = import.meta.env.VITE_PLAUSIBLE_DOMAIN
-const plausibleHost = import.meta.env.VITE_PLAUSIBLE_HOST
-
-console.log("mgApiUrl: ")
-console.log(mgApiUrl)
-console.log(import.meta.env.VITE_PLAUSIBLE_HOST)
-console.log(import.meta.env.VITE_PLAUSIBLE_DOMAIN)
-
-if (plausibleDomain == "" || plausibleHost == "") {
-    throw new Error('Plausible could not be initialized!');
-}
-
-const { trackPageview } = Plausible({
-    apiHost: plausibleHost,
-    domain: plausibleDomain,
-    trackLocalhost: true,
-})
-trackPageview()
 
 const app = createApp(App);
 app.use(router);
